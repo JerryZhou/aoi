8839b730d591fcea71106087b7ae357cde7d59d1
JerryZhou <JerryZhou@outlook.com>
2016-03-13 00:00:13 +0800

    add souce code about navigation mesh

---
+++ navi.c
@@ -0,0 +1,15 @@
+/*!
+  Navigation Mesh In Game Developing
+  Copyright Jerryzhou@outlook.com
+Licence: Apache 2.0
+
+Project: https://github.com/JerryZhou/aoi.git
+Purpose: Resolve the Navigation Problem In Game Developing
+with high run fps
+with minimal memory cost,
+
+Please see examples for more details.
+*/
+
+#include "navimesh.h"
+
---
+++ navi.h
@@ -0,0 +1,19 @@
+#ifndef __NAVI_H_
+#define __NAVI_H_
+
+/*iref iarray islice ivec2 ivec3*/
+#include "aoi.h"
+
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif /*end of __cplusplus*/
+
+
+#ifdef __cplusplus
+}
+#endif /*end of __cplusplus*/
+
+#endif /*end of __NAVI_H_*/
+
+
