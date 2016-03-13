e1523a5d2dcc9946405e20252e7eaa352a3317b8
JerryZhou <JerryZhou@outlook.com>
2016-03-13 13:20:59 +0800

    add docs

--- navi.h
+++ navi.h
@@ -9,6 +9,30 @@
 extern "C" {
 #endif /*end of __cplusplus*/
 
+/*
+ * 1. Nodal-pathfinding:
+ * http://www.jgallant.com/nodal-pathfinding-in-unity-2d-with-a-in-non-grid-based-games/
+ *
+ * 2. A* algorithm:
+ * http://www.policyalmanac.org/games/aStarTutorial.htm
+ * https://en.wikipedia.org/wiki/A*_search_algorithm
+ * https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+ *
+ * 3. Unit-Movement
+ * http://www.gamasutra.com/view/feature/131720/coordinated_unit_movement.php
+ *
+ * 4. Navigation-Mesh
+ * https://en.wikipedia.org/wiki/Navigation_mesh
+ * http://accu.org/index.php/journals/1838
+ *      [Automatic Navigation Mesh Generation in Configuration Space] 
+ * http://www.staff.science.uu.nl/~gerae101/pdf/CAVW_Dynamic_ECM.pdf
+ *      [A navigation mesh for dynamic environments]
+ *
+ * 5. AI
+ * http://www.kbs.twi.tudelft.nl/docs/MSc/2001/Waveren_Jean-Paul_van/thesis.pdf 
+ *      [Quake III Bot System]
+ * */
+
 
 #ifdef __cplusplus
 }
