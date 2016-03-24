e58d1bf2a405bfebb5617ba96607a47154432210
JerryZhou <JerryZhou@outlook.com>
2016-03-25 01:52:39 +0800

    add iringbuffer

--- aoi.c
+++ aoi.c
@@ -284,6 +284,15 @@ int inextpot(int x) {
     return x + 1;
 }
 
+/* sleeping the current thread */
+void isleep(unsigned int milliseconds) {
+#ifdef WIN32
+    Sleep((DWORD)milliseconds);
+#else
+    sleep(milliseconds);
+#endif
+}
+
 /* zero point */
 const ipos kipos_zero = {0,0};
 
@@ -2694,6 +2703,261 @@ double istringatof(const istring s) {
     }
     
     return atof(buf);
+}
+
+/*************************************************************/
+/* iringbuffer                                               */
+/*************************************************************/
+
+static void _iringbuffer_entry_free(iref *ref) {
+    iringbuffer *r = icast(iringbuffer, ref);
+    iarrayfree(r->buf);
+    iobjfree(r);
+}
+
+/* Make a ring buffer */
+iringbuffer *iringbuffermake(size_t capacity, int flag) {
+    iringbuffer *r = iobjmalloc(iringbuffer);
+    r->buf = iarraymakechar(capacity+1);
+    r->buf->len = capacity;
+    r->free = _iringbuffer_entry_free;
+    r->flag = flag;
+    
+    iretain(r);
+    return r;
+}
+
+/* release the ring buffer */
+void iringbufferfree(iringbuffer *r) {
+    irelease(r);
+}
+
+/* close the ring buffer: can not read and write */
+void iringbufferclose(iringbuffer *r) {
+    r->flag |= EnumRingBufferFlag_ReadChannelShut;
+    r->flag |= EnumRingBufferFlag_WriteChannelShut;
+}
+
+/* shutdown the ringbuffer, to forbid the ringbuffer behavior */
+void iringbuffershut(iringbuffer *r, int channel) {
+    r->flag |= channel;
+}
+
+/* write s to ringbuffer, return count of write */
+size_t iringbufferwrite(iringbuffer *rb, const char* s, size_t length) {
+    size_t empty;
+    size_t write;
+    size_t finish = 0;
+    size_t content;
+    size_t capacity = iarraycapacity(rb->buf)-1;
+    
+    /* write to shut down rb */
+    if (rb->flag & EnumRingBufferFlag_WriteChannelShut) {
+        return -1;
+    }
+    
+    if (finish < length) do {
+        /* should break when got shut down */
+        if (rb->flag & EnumRingBufferFlag_WriteChannelShut) {
+            break;
+        }
+        /* current content size */
+        content = rb->wlen - rb->rlen;
+        
+        /* overide buffer */
+        if (rb->flag & EnumRingBufferFlag_Override) {
+            empty = length - finish;
+        } else {
+            empty = imin(capacity - content, length - finish);
+        }
+        
+        /* no space continue */
+        if (empty == 0) {
+            /* the write channel have been shutdown */
+            if (rb->flag & EnumRingBufferFlag_WriteChannelShut) {
+                break;
+            }
+            /* not block mode, will return right now */
+            if (!(rb->flag & EnumRingBufferFlag_BlockWrite)) {
+                break;
+            }
+            /* need sleep */
+            if (rb->flag & EnumRingBufferFlag_WriteSleep) {
+                isleep(0);
+            }
+            /* can be dynamic resize the space */
+            continue;
+        }
+        
+        /* write data */
+        if (empty > 0) do {
+            write = capacity - rb->wcursor;
+            write = imin(write, empty);
+            
+            memcpy(rb->buf->buffer + rb->wcursor, s + finish, write);
+            rb->wcursor += write;
+            rb->wlen += write;
+            if (rb->wcursor >= capacity) {
+                rb->wcursor = 0;
+            }
+            
+            finish += write;
+            empty -= write;
+        } while(empty > 0);
+        
+    } while(finish < length && (rb->flag & EnumRingBufferFlag_BlockWrite));
+    
+    return finish;
+}
+
+/* read to dst, until read full of dst, return the realy readed count */
+size_t iringbufferread(iringbuffer *rb, char * dst, size_t length) {
+    size_t full;
+    size_t read;
+    size_t finish = 0;
+    size_t capacity = iarraycapacity(rb->buf)-1;
+    
+    /* write to shut down rb */
+    if (rb->flag & EnumRingBufferFlag_ReadChannelShut) {
+        return -1;
+    }
+    
+    if (finish < length) do {
+        /* write to shut down rb */
+        if (rb->flag & EnumRingBufferFlag_ReadChannelShut) {
+            break;
+        }
+        
+        /* write override it */
+        if (rb->flag & EnumRingBufferFlag_Override) {
+            full = length - finish;
+        } else {
+            full = imin((rb->wlen - rb->rlen), length - finish);
+        }
+        
+        /* no content continue */
+        if (full == 0) {
+            /* the write channel have been shutdown */
+            if (rb->flag & EnumRingBufferFlag_WriteChannelShut) {
+                break;
+            }
+            /* the read operating is not block, will return right now */
+            if (!(rb->flag & EnumRingBufferFlag_BlockRead)) {
+                break;
+            }
+            /* need sleep */
+            if (rb->flag & EnumRingBufferFlag_ReadSleep) {
+                isleep(0);
+            }
+            /* can be dynamic resize the space */
+            continue;
+        }
+        
+        /* read data */
+        if (full > 0) do {
+            read = capacity - rb->rcursor;
+            read = imin(read, full);
+            
+            memcpy(dst + finish, rb->buf->buffer + rb->rcursor, read);
+            rb->rcursor += read;
+            rb->rlen += read;
+            if (rb->rcursor >= capacity) {
+                rb->rcursor = 0;
+            }
+            
+            finish += read;
+            full -= read;
+        } while(full > 0);
+        
+    } while(finish < length && (rb->flag & EnumRingBufferFlag_BlockRead));
+    
+    return finish;
+}
+
+/* return the ready count of content */
+size_t iringbufferready(iringbuffer *r) {
+    return r->wlen - r->rlen;
+}
+
+/* giving the raw buffer address, unsafe behavior */
+const char* iringbufferraw(iringbuffer *r) {
+    return (const char*)iarraybuffer(r->buf);
+}
+
+/* Print to rb: support
+ * %s(c null end string),
+ * %i(signed int),
+ * %I(signed 64 bit),
+ * %u(unsigned int),
+ * %U(unsigned 64bit) */
+size_t iringbufferfmt(iringbuffer *rb, const char * fmt, ...) {
+    const char *f = fmt;
+    size_t i;
+    va_list ap;
+    
+    char next, *str;
+    size_t l;
+    int64_t num;
+    uint64_t unum;
+    
+    char buf[_IRB_LLSTR_SIZE];
+    
+    va_start(ap,fmt);
+    f = fmt;    /* Next format specifier byte to process. */
+    i = 0;
+    while(*f) {
+        
+        /* Make sure there is always space for at least 1 char. */
+        switch(*f) {
+            case '%':
+                next = *(f+1);
+                f++;
+                switch(next) {
+                    case 's':
+                    case 'S':
+                        str = va_arg(ap,char*);
+                        l = strlen(str);/*(next == 's') ?  : sdslen(str); */
+                        iringbufferwrite(rb, str, l);
+                        i += l;
+                        break;
+                    case 'i':
+                    case 'I':
+                        if (next == 'i')
+                            num = va_arg(ap,int);
+                        else
+                            num = va_arg(ap,int64_t);
+                    {
+                        l = _ill2str(buf,num);
+                        iringbufferwrite(rb, buf, l);
+                        i += l;
+                    }
+                        break;
+                    case 'u':
+                    case 'U':
+                        if (next == 'u')
+                            unum = va_arg(ap,unsigned int);
+                        else
+                            unum = va_arg(ap,uint64_t);
+                    {
+                        l = _iull2str(buf,unum);
+                        iringbufferwrite(rb, buf, l);
+                        i += l;
+                    }
+                        break;
+                    default: /* Handle %% and generally %<unknown>. */
+                        iringbufferwrite(rb, f, 1);
+                        break;
+                }
+                break;
+            default:
+                iringbufferwrite(rb, f, 1);
+                break;
+        }
+        f++;
+    }
+    va_end(ap);
+    
+    return i;
 }
 
 /*************************************************************/
--- aoi.h
+++ aoi.h
@@ -28,6 +28,8 @@
 #define snprintf _snprintf
 typedef _int64 int64_t;
 typedef _uint64 uint64_t;
+typedef _int32 int32_t;
+typedef _uint32 uint32_t;
 
 #ifndef offsetof
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
@@ -43,6 +45,7 @@ typedef _uint64 uint64_t;
 #include <stdbool.h>
 #include <inttypes.h>
 #include <sys/time.h>
+#include <unistd.h>
 #endif /* end of: _WIN32 */
 
 /*****
@@ -137,6 +140,9 @@ typedef unsigned char ibool;
     
 /* return the next pow of two */
 int inextpot(int size);
+    
+/* sleeping the current thread */
+void isleep(unsigned int milliseconds);
 
 /*************************************************************/
 /* ipos                                                      */
@@ -165,7 +171,6 @@ extern const ipos3 kipos3_zero;
     
 /* 计算距离的平方 */
 ireal idistancepow3(const ipos3 *p, const ipos3 *t);
-
 
 /*************************************************************/
 /* ivec2                                                     */
@@ -538,6 +543,7 @@ int imetaregister(const char* name, int size, int capacity);
     __ideclaremeta(irefautoreleasepool, 0),   \
     __ideclaremeta(iarray, 0),                \
     __ideclaremeta(islice, 0),                \
+    __ideclaremeta(iringbuffer, 0),           \
     __ideclaremeta(ipolygon3d, 0),            \
     __ideclaremeta(ipolygon2d, 0)
     
@@ -1186,6 +1192,75 @@ int istringatoi(const istring s);
  */
 double istringatof(const istring s);
     
+    
+/*************************************************************/
+/* iringbuffer                                               */
+/*************************************************************/
+/* ringbuffer control flags */
+typedef enum EnumRingBufferFlag {
+    EnumRingBufferFlag_BlockRead = 1,  /* Block Read Will Got What We Needed */
+    EnumRingBufferFlag_BlockWrite = 1<<1, /* Block Write Util We Got Empty Spaces */
+    
+    EnumRingBufferFlag_Override = 1<<2,   /* Override The Ring Buffer */
+    
+    EnumRingBufferFlag_ReadChannelShut = 1<<3,
+    EnumRingBufferFlag_WriteChannelShut = 1<<4,
+    
+    EnumRingBufferFlag_ReadSleep = 1<<5,
+    EnumRingBufferFlag_WriteSleep = 1<<6,
+}EnumRingBufferFlag;
+    
+/* ring buffer */
+/*[the ring buffer](https://github.com/JerryZhou/ringbuffer)*/
+typedef struct iringbuffer {
+    irefdeclare;
+    
+    /* char array */
+    iarray *buf;
+    /* the control flags */
+    int flag;
+    /* write cursor */
+    size_t wcursor;
+    /* read cursor */
+    size_t rcursor;
+    /* total write length */
+    int64_t wlen;
+    /* total read length */
+    int64_t rlen;
+} iringbuffer;
+    
+/* Make a ring buffer */
+iringbuffer *iringbuffermake(size_t capacity, int flag);
+
+/* release the ring buffer */
+void iringbufferfree(iringbuffer *r);
+
+/* close the ring buffer: can not read and write */
+void iringbufferclose(iringbuffer *r);
+
+/* shutdown the ringbuffer, to forbid the ringbuffer behavior */
+void iringbuffershut(iringbuffer *r, int channel);
+
+/* write s to ringbuffer, return count of write */
+size_t iringbufferwrite(iringbuffer *r, const char* s, size_t len);
+
+/* read to dst, until read full of dst, return the realy readed count */
+size_t iringbufferread(iringbuffer *r, char * dst, size_t len);
+
+/* return the ready count of content */
+size_t iringbufferready(iringbuffer *r);
+
+/* giving the raw buffer address, unsafe behavior */
+const char* iringbufferraw(iringbuffer *r);
+
+/* Print to rb: support
+ * %s(c null end string),
+ * %i(signed int),
+ * %I(signed 64 bit),
+ * %u(unsigned int),
+ * %U(unsigned 64bit) */
+size_t iringbufferfmt(iringbuffer *rb, const char * fmt, ...);
+
 /*************************************************************/
 /* ipolygon3d                                                */
 /*************************************************************/
