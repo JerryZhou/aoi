3c74af89f54cc257f4c8c37fd980108a1f51d8d5
jerry <jerryzhou@outlook.com>
2018-09-12 22:08:51 +0800

    support lua 5.3

--- lua-bind/laoi.c
+++ lua-bind/laoi.c
@@ -77,6 +77,17 @@
 # define __iallmeta(XX)
 #endif
 
+#if LUA_VERSION_NUM >= 503
+/* one can simply enable LUA_COMPAT_5_2 to be backward compatible.
+However, this does not work when we are trying to use system-installed lua,
+hence these redefines
+*/
+#define luaL_optlong(L,n,d)     ((long)luaL_optinteger(L, (n), (d)))
+#define luaL_optint(L,n,d)  ((int)luaL_optinteger(L, (n), (d)))
+#define luaL_checklong(L,n)     ((long)luaL_checkinteger(L, (n)))
+#define luaL_checkint(L,n)      ((int)luaL_checkinteger(L, (n)))
+#endif
+
 
 /* {{ map */
 
